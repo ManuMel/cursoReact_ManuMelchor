ffc79eb28c8d63c5ac63ecc79bf63e17985ee6f3
Manu <jm_editor@hotmail.com>
2022-07-19 12:39:53 -0500

    Clase 02 - JSX & transpiling

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+
+function App() {
+    return (
+        <>
+            <header id="header">
+                <NavBar/>
+            </header>
+        </>
+    )
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar() {
+    return (
+        <nav>
+            <h1>React store</h1>
+            <a href="#">productos</a>
+            <a href="#">sucursales</a>
+            <a href="#">carrito</a>
+            <a href="#">contacto</a>
+        </nav>
+    )
+}
+
+export default NavBar;
